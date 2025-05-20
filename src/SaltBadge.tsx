e9ff715c7726ec07c2d47a35836adddad02fa879
Charlesincharge43 <charles.m.long43@gmail.com>
2025-05-20 16:26:00 -0500

    Update SaltBadge.tsx remove comments

--- src/SaltBadge.tsx
+++ src/SaltBadge.tsx
@@ -9,19 +9,3 @@ const SaltBadge = () => (
 )
 
 export default SaltBadge
-
-/*
-So far:
-
-direct import and using the modules fail
-
-import '@salt-ds/core'   fails
-
-dynamic import  (import('...')) succeeds
-
-if module federation removes.. the succeeds
-
-(or salt-ds/core is removed from shared)
-
-
-*/
